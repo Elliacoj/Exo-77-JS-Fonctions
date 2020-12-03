846e68e0358f3b368b8040950f4944632acef8d7
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-03 11:16:25 +0100

    Modif

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,11 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
 
-
+function text(){
+    return "Le texte ajouté"
+}
 
 /* Appeler la fonction */
-
+document.getElementById("container").innerHTML = text();
 
 
 
